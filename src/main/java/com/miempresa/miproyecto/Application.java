bd5e1b170a3b54c674b1a2cdc5bc7bcef93a8b92
Carlos Paredes <cardan1990@hotmail.com>
2023-10-05 16:15:28 +0200

    Renamed app file name

--- src/main/java/com/miempresa/miproyecto/Application.java
+++ src/main/java/com/miempresa/miproyecto/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MiProyectoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MiProyectoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
